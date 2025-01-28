f2e8a64a791c245da29c60e5f58ae140df8ef0a1
tugalsan karabacak <tugalsan@users.noreply.github.com>
2025-01-28 10:29:54 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,18 +1,18 @@
 module com.tugalsan.api.file.xlsx {
+    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
+    requires org.apache.poi.ooxml.schemas;
+    requires org.apache.poi.scratchpad;
+
     requires java.desktop;
-    requires poi;
-    requires poi.ooxml;
     requires com.tugalsan.api.list;
     requires com.tugalsan.api.url;
     requires com.tugalsan.api.charset;
     requires com.tugalsan.api.union;
     requires com.tugalsan.api.cast;
     requires com.tugalsan.api.unsafe;
-    
     requires com.tugalsan.api.math;
-    
     requires com.tugalsan.api.function;
-    
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.string;
     requires com.tugalsan.api.file;
